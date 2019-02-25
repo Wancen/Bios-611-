30e420e0e6e4408a2dcd294145d1e10c3e2166f7
Wancen <42752661+Wancen@users.noreply.github.com>
2019-02-24 19:48:23 -0500

    Delete Combine_csv.R

--- Combine_csv.R
+++
@@ -1,9 +0,0 @@
-#Clean title
-args = commandArgs(trailingOnly = TRUE)
-
-a<-read.csv(args[1],header = FALSE)
-b<-unique(a)
-b<-b[2:nrow(b),]
-colnames(b)<-c("collaborator","title","abstract")
-
-write.csv(b,file("Summary.csv"),row.names=FALSE)
